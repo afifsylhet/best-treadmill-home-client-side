31496feab292d35b43504231dea68ff303aa5991
Afif Ahmed <afif.ahmed@yahoo.com>
2021-11-10 14:19:41 +0600

    Basic folder structure created

---
+++ src/fake data/reviews.js
@@ -0,0 +1,51 @@
+const reviews = [
+
+    {
+        sl: 1,
+        rating: 4.5,
+        rev_name: "Cindy Barsa",
+        rev_img: "https://i.ibb.co/gjqSwyX/Client-2.jpg",
+        rev_address: "Madrid, Spain",
+        rev_text: "What a wonderful experience! The two tour leaders who were archaeologists enthralled us with the history of the Islands. The food was bountiful. On the boat the meals were wholesome and delicious. We were treated to wonderful meals on the the Islands that let us sample each Island's speciality. There was free time on each of the Islands that allowed for shopping, exploring or sitting round watching island life sipping a drink! The accommodation on the gulet was more than adequate with rooms being"
+    },
+    {
+        sl: 2,
+        rating: 5.00,
+        rev_name: "Lura Page",
+        rev_img: "https://i.ibb.co/vZ8F9dV/Client-1.jpg",
+        rev_address: "Snjee, Singapur",
+        rev_text: "What a wonderful experience! The two tour leaders who were archaeologists enthralled us with the history of the Islands. The food was bountiful. On the boat the meals were wholesome and delicious. We were treated to wonderful meals on the the Islands that let us sample each Island's speciality. There was free time on each of the Islands that allowed for shopping, exploring or sitting round watching island life sipping a drink! The accommodation on the gulet was more than adequate with rooms being"
+    },
+    {
+        sl: 3,
+        rating: 4.50,
+        rev_name: "Mike Hanson",
+        rev_img: "https://i.ibb.co/GxzX8m1/Client-4.jpg",
+        rev_address: "Baising, China",
+        rev_text: "What a wonderful experience! The two tour leaders who were archaeologists enthralled us with the history of the Islands. The food was bountiful. On the boat the meals were wholesome and delicious. We were treated to wonderful meals on the the Islands that let us sample each Island's speciality. There was free time on each of the Islands that allowed for shopping, exploring or sitting round watching island life sipping a drink! The accommodation on the gulet was more than adequate with rooms being"
+    },
+    {
+        sl: 4,
+        rating: 5.00,
+        rev_name: "Ela Brown",
+        rev_img: "https://i.ibb.co/X2MzJRh/Client-3.jpg",
+        rev_address: "London, UK",
+        rev_text: "What a wonderful experience! The two tour leaders who were archaeologists enthralled us with the history of the Islands. The food was bountiful. On the boat the meals were wholesome and delicious. We were treated to wonderful meals on the the Islands that let us sample each Island's speciality. There was free time on each of the Islands that allowed for shopping, exploring or sitting round watching island life sipping a drink! The accommodation on the gulet was more than adequate with rooms being"
+    },
+    {
+        sl: 5,
+        rating: 5.00,
+        rev_name: "Heallry Jems",
+        rev_img: "https://i.ibb.co/SxFKmyC/Client-5.jpg",
+        rev_address: "Kolkata, India",
+        rev_text: "What a wonderful experience! The two tour leaders who were archaeologists enthralled us with the history of the Islands. The food was bountiful. On the boat the meals were wholesome and delicious. We were treated to wonderful meals on the the Islands that let us sample each Island's speciality. There was free time on each of the Islands that allowed for shopping, exploring or sitting round watching island life sipping a drink! The accommodation on the gulet was more than adequate with rooms being"
+    },
+    {
+        sl: 6,
+        rating: 4.50,
+        rev_name: "Steve Huyen",
+        rev_img: "https://i.ibb.co/p32qdxv/Client-6.jpg",
+        rev_address: "New York, USA",
+        rev_text: "What a wonderful experience! The two tour leaders who were archaeologists enthralled us with the history of the Islands. The food was bountiful. On the boat the meals were wholesome and delicious. We were treated to wonderful meals on the the Islands that let us sample each Island's speciality. There was free time on each of the Islands that allowed for shopping, exploring or sitting round watching island life sipping a drink! The accommodation on the gulet was more than adequate with rooms being"
+    }
+]
